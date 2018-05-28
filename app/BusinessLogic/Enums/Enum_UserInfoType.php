eb2042a4d4f00b37cac3dce040f2a120d23de4d6
hamisor <tanghanlu@hotmail.com>
2018-05-28 17:24:53 +1000

    test formating reset

--- app/BusinessLogic/Enums/Enum_UserInfoType.php
+++ app/BusinessLogic/Enums/Enum_UserInfoType.php
@@ -7,8 +7,8 @@ use MyCLabs\Enum\Enum;
 
 class Enum_UserInfoType extends Enum
 {
-	public const USER_BIO			= "USER_BIO";
-	public const USER_EDUCATION		= "USER_EDUCATION";
-	public const USER_SKILLS		= "USER_SKILLS";
-	public const USER_WORK_EXPERIENCE	= "USER_WORK_EXPERIENCE";
+  public const USER_BIO				= "USER_BIO";
+  public const USER_EDUCATION		= "USER_EDUCATION";
+  public const USER_SKILLS			= "USER_SKILLS";
+  public const USER_WORK_EXPERIENCE = "USER_WORK_EXPERIENCE";
 }
